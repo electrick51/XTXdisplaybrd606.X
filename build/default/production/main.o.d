3c8b9dd7ecc411a217b75864bc9275008cc80b5d
Rick <rickgs51@charter.net>
2018-05-01 07:55:26 -0500

    initial commit

---
+++ build/default/production/main.o.d
@@ -0,0 +1,10 @@
+build/default/production/main.o:  \
+main.c  \
+mcc_generated_files/mcc.h  \
+ mcc_generated_files/pin_manager.h  \
+mcc_generated_files/uart2.h  \
+ mcc_generated_files/uart3.h  \
+mcc_generated_files/tmr2.h  \
+ mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/traps.h  \
+ mcc_generated_files/tmr3.h 
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=XTXdisplaybrd606.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/XTXdisplaybrd606.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=xtxdisplaybrd606.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xtxdisplaybrd606.x.tar
